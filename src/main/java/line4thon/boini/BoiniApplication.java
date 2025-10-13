0c7855d5cdcf1954093f30dd87ad24543ca562e6
chaeelin <chaeee0123@gmail.com>
2025-10-13 20:01:28 +0900

    :tada: Start: 프로젝트 생성

---
+++ src/main/java/line4thon/boini/BoiniApplication.java
@@ -0,0 +1,13 @@
+package line4thon.boini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoiniApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BoiniApplication.class, args);
+  }
+
+}
---
+++ src/test/java/line4thon/boini/BoiniApplicationTests.java
@@ -0,0 +1,13 @@
+package line4thon.boini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoiniApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
