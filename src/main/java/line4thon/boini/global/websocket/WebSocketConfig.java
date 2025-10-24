76665c2b74d23027b0f108dc9e07f9e6e081e792
chaeelin <chaeee0123@gmail.com>
2025-10-25 02:21:28 +0900

    :sparkles: Feat: WebsocketConfig 추가

---
+++ src/main/java/line4thon/boini/global/websocket/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package line4thon.boini.global.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws")
+        .setAllowedOriginPatterns("*")
+        .withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topic");  // 서버 → 클라 (broadcast)
+    registry.setApplicationDestinationPrefixes("/app"); // 클라 → 서버
+  }
+}
