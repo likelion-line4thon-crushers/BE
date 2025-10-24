1503c8a65e60d96b8a13f3e3381c3d80f6d073fd
chaeelin <chaeee0123@gmail.com>
2025-10-25 02:22:32 +0900

    :sparkles: Feat: 인증/인가 관련 에러 코드 추가

--- src/main/java/line4thon/boini/global/common/exception/GlobalErrorCode.java
+++ src/main/java/line4thon/boini/global/common/exception/GlobalErrorCode.java
@@ -11,7 +11,12 @@ public enum GlobalErrorCode implements BaseErrorCode {
 
   INVALID_INPUT_VALUE("G001", "유효하지 않은 입력입니다.", HttpStatus.BAD_REQUEST),
   RESOURCE_NOT_FOUND("G002", "요청한 리소스를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-  INTERNAL_SERVER_ERROR("G003", "서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+  INTERNAL_SERVER_ERROR("G003", "서버 내부 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+
+  // 인증 / 인가 관련
+  UNAUTHORIZED("A001", "인증이 필요합니다. (JWT 토큰이 존재하지 않거나 유효하지 않습니다.)", HttpStatus.UNAUTHORIZED),
+  FORBIDDEN("A002", "해당 리소스에 접근할 권한이 없습니다.", HttpStatus.FORBIDDEN);
+
 
   private final String code;
   private final String message;
