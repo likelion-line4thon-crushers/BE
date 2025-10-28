e628a388884e97a61fe3a4ef53bdfcaf5d645fc7
chaeelin <chaeee0123@gmail.com>
2025-10-28 22:36:49 +0900

    :sparkles: Feat: 방 옵션 제거

--- src/main/java/line4thon/boini/presenter/room/dto/request/CreateRoomRequest.java
+++ src/main/java/line4thon/boini/presenter/room/dto/request/CreateRoomRequest.java
@@ -1,7 +1,6 @@
 package line4thon.boini.presenter.room.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import line4thon.boini.presenter.room.model.RoomOptions;
 import lombok.*;
 
 @Getter
@@ -10,8 +9,6 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CreateRoomRequest {
-  @NotNull
-  private RoomOptions options;
   @NotNull
   private Integer count;
 }
