3ebfbd96ce3651fb9f838c9daa78c9c970996fad
chaeelin <chaeee0123@gmail.com>
2025-11-07 01:24:01 +0900

    :sparkles: Feat: DB 저장 기능 추가

--- src/main/java/line4thon/boini/audience/feedback/service/FeedbackService.java
+++ src/main/java/line4thon/boini/audience/feedback/service/FeedbackService.java
@@ -3,6 +3,8 @@ package line4thon.boini.audience.feedback.service;
 import java.time.Instant;
 import line4thon.boini.audience.feedback.dto.request.CreateFeedbackRequest;
 import line4thon.boini.audience.feedback.dto.response.CreateFeedbackResponse;
+import line4thon.boini.audience.feedback.entity.FeedbackEntity;
+import line4thon.boini.audience.feedback.repository.FeedbackRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,25 +12,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FeedbackService {
 
-  // private final FeedbackRepository feedbackRepository; // 실제 저장시 주입
+   private final FeedbackRepository feedbackRepository;
 
   public CreateFeedbackResponse write(String roomId, CreateFeedbackRequest req) {
 
-    long id = System.currentTimeMillis();
     Instant now = Instant.now();
 
-    // FeedbackEntity entity = FeedbackEntity.builder()
-    //     .id(id)
-    //     .roomId(roomId)
-    //     .audienceId(req.getAudienceId())
-    //     .rating(req.getRating())
-    //     .comment(req.getComment())
-    //     .createdAt(now)
-    //     .build();
-    // feedbackRepository.save(entity);
+    FeedbackEntity entity = FeedbackEntity.builder()
+        .roomId(roomId)
+        .audienceId(req.getAudienceId())
+        .rating(req.getRating())
+        .comment(req.getComment())
+        .createdAt(now)
+        .build();
+
+    FeedbackEntity saved = feedbackRepository.save(entity);
 
     return CreateFeedbackResponse.builder()
-        .id(id)
+        .id(saved.getId())
         .roomId(roomId)
         .audienceId(req.getAudienceId())
         .rating(req.getRating())
