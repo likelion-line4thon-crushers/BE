5e7d8986f90bc272558035727dab95158ea5c95d
Son <suruna1026@gmail.com>
2025-11-08 21:49:04 +0900

    ✨ ãFeat: 리ì 각 스티커커 별로 가장 많이 받은 슬라이드 반환

---
+++ src/main/java/line4thon/boini/presenter/aiReport/dto/request/MostReactionStickerRequest.java
@@ -0,0 +1,11 @@
+package line4thon.boini.presenter.aiReport.dto.request;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+public class MostReactionStickerRequest {
+}
